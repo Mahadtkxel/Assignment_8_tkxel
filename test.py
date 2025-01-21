2b371e9a797f63f41623e67e4e50935c00841f57
mahadtkxel <mahad.karim@tkxel.io>
2025-01-21 10:27:58 +0500

    added db & changed port

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def test():
@@ -8,6 +16,6 @@ def test():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
     
     
